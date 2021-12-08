488f0e1df554aae579bc9b665b426963b1cda54c
Moritz Eich <hey@moritz.dev>
2021-12-08 22:16:29 +0000

    add: 'leaky integrator'

---
+++ models/LI.py
@@ -0,0 +1,18 @@
+from models.BaseNeuronModel import BaseNeuronModel
+
+
+class LI(BaseNeuronModel):
+    def step(self, input_current: float):
+        v = self.voltages[-1]
+
+        # check if voltage is above threshold
+        if v >= self.v_th:
+            self.spike_times += [len(self.voltages) - 1]
+            v = self.v_reset
+
+        # calculate membrane voltage change
+        dv = (self.dt / self.tau_m) + (input_current / self.R)
+
+        v += dv
+
+        self.voltages += [v]
--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
+from .LI import LI
 from .LIF import LIF
 from .LIF_Ref_Period import LIF_Ref_Period
 from .LIF_Spike_Rate_Adaption import LIF_Spike_Rate_Adaption
